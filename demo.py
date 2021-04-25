a8f28e512e723003abbb3dc3b43fcfe19d8082ac
Rupert Wu <wu.rupert@outlook.com>
2021-04-24 21:23:45 -0400

    Added some interesting experiments.

--- demo.py
+++ demo.py
@@ -1,16 +1,12 @@
 from lib.model.filter import run_simulation_top_k
 from test.simulations import simulations
+from os import mkdir, path
 import sys
 
 experiment = sys.argv[1]
-k, conn_const = 5, 1
-if len(sys.argv) > 2:
-    k = int(sys.argv[2])
-if len(sys.argv) > 3:
-    conn_const = int(sys.argv[3])
+output_dir = "experiments/{}".format(experiment)
+if not path.exists(output_dir):
+    mkdir(output_dir)
 
 stations = simulations[experiment]
-
-output_dir = "images"
-output_dir = None
-ll = run_simulation_top_k(stations, k, verbose=True, output_dir=output_dir)
+ll = run_simulation_top_k(stations, 5, output_dir, verbose=True)
--- test/simulations.py
+++ test/simulations.py
@@ -12,8 +12,50 @@ def generate_cycle(stations):
 
 
 simulations = {
-    "line_1": Line1Wrapped[:],
-    "line_2": Line2Wrapped[:],
-    "line_3": Line2Wrapped[:],
-    "line_4": Line2Wrapped[:],
+    # Normal experiments.
+    "line_1":
+        Line1Wrapped[:],
+    "line_2":
+        Line2Wrapped[:],
+    "line_3":
+        Line3Wrapped[:],
+    "line_4":
+        Line4Wrapped[:],
+    "line_1_interior":
+        Line1Wrapped[1:-1],
+    "line_2_interior":
+        Line2Wrapped[1:-1],
+    "line_3_interior":
+        Line3Wrapped[1:-1],
+    "line_4_interior":
+        Line4Wrapped[1:-1],
+
+    # More interesting experiments.
+    "around_kennedy_3": [
+        LawrenceEast[1],
+        Kennedy3[0],
+        LawrenceEast[0],
+        Ellesmere[0],
+    ],
+    "around_stgeorge": [Spadina2[0], StGeorge2[0], StGeorge1[1], Spadina1[1]],
+    "around_union": [
+        StAndrew[0],
+        Union[0],
+        StAndrew[1],
+    ],
+    "yorkville": [
+        Sherbourne[1], BloorYonge2[1], Bay[1], BloorYonge2[0], Sherbourne[0],
+        BloorYonge2[1], Bay[1], StGeorge2[1], Spadina2[1]
+    ],
+    "downtown_u": [
+        # Museum[0],
+        QueensPark[0],
+        StPatrick[0],
+        Osgoode[0],
+        StAndrew[0],
+        Union[0],
+        King[0],
+        Queen[0],
+        Dundas[0],
+    ],
 }
